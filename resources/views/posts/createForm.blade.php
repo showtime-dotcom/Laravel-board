9b3bb60dae9ec2be890c92d64461f40b6b3eb040
showtime-dotcom <shota57brother@gmail.com>
2025-07-12 04:27:05 +0900

    update20250712: 個人課題の修正依頼分

--- resources/views/posts/createForm.blade.php
+++ resources/views/posts/createForm.blade.php
@@ -12,6 +12,10 @@
     <!-- 名前を変数newName、投稿内容を変数newPostとしてPostController.phpのcreateメソッドへpost形式で送る -->
     <!-- {!! Form::input('name', 'newName', null, ['required', 'class' => 'form-control', 'placeholder' => '名前']) !!} -->
     {!! Form::input('text', 'contents', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容']) !!}
+
+    @error('contents')
+    <div style="color: red;">{{ $message }}</div>
+    @enderror
   </div>
 
   <!-- ↓送信ボタン -->
